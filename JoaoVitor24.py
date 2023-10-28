df87247d00bf380b1dc0f65e744903669144de15
João Vitor Muniz Vasconcelos <140927075+JoaoVitorMuniz16@users.noreply.github.com>
2023-10-27 21:35:32 -0300

    Add files via upload

---
+++ JoaoVitor24.py
@@ -0,0 +1,6 @@
+# esse eu tive que ver um Video Tutorial pra ver como faz... Mas valeu a Pena ;D
+
+num = int(input('Digite um numero para ver a sua tabuada: '))
+
+for c in range(0,11):
+    print('{} X {} = {}'.format(num ,c,(num * c)))
